3c7599fb29af79f5221ddcca8c286eb19d7f142e
chikkuu131 <164413198+chikkuu131@users.noreply.github.com>
2024-03-22 02:56:20 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Set kth bit/set-kth-bit.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setKthBit(int N, int K)
+    {
+        // Write Your Code here
+        // jai ganesh
+        
+        
+        //Shifting 1 to left K times to create a mask with only the Kth bit set,
+        //then ORing it with N to set the Kth bit.
+        return ((1 << K) | N);
+    }
+    
+};
+
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N, K;
+        cin >> N >> K;
+        
+        Solution ob;
+        int ans = ob.setKthBit(N,K);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
